b309edb8048bf3a4105aa0d7a949db9ff00460ee
guomengjie <guomengjie@qudoor.cn>
2022-09-20 09:43:16 +0800

    feat: add QuSaas backend, including authorization. QuTrunk -> QuSaas -> QuSprout,

--- qutrunk/backends/__init__.py
+++ qutrunk/backends/__init__.py
@@ -3,6 +3,6 @@ Contains back-ends for qutrunk.
 """
 from .backend import Backend
 from .local import BackendLocal
+from .qusaas import BackendQuSaas
 from .qusprout import BackendQuSprout, ExecType
 from .ibm import BackendIBM
-
