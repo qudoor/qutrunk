cc286adc540f9aa4917991dc05d114ed77e7e2a7
liuli <liuli@qudoor.com>
2022-09-22 16:58:31 +0800

    Update test case

--- qutrunk/test/test_amplitudes.py
+++ qutrunk/test/test_amplitudes.py
@@ -9,30 +9,33 @@ from qutrunk.test.gate.check_all_state import check_all_state
 def test_amplitudes_local():
     """test set amp for local"""
     # 使用本地量子计算模拟器
+    qubit_len = 3
     circuit = QCircuit()
-    qr = circuit.allocate(2)
-    QSP("AMP", [1, 2, 3]) * qr
+    qr = circuit.allocate(qubit_len)
+    QSP("AMP", [1-2j, 2+3j, 3-4j, 0.5+0.7j]) * qr
 
-    res = []
+    res = ['0, 0'] * (2**qubit_len)
     for i in range(len(circuit.init_amp_reals)): 
-        res.append(','.join([str(circuit.init_amp_reals[i]), str(circuit.init_amp_imags[i])]))
+        res[i] = ','.join([str(circuit.init_amp_reals[i]), str(circuit.init_amp_imags[i])])
 
     res_box = circuit.get_all_state()
 
     # 检查数据是否一致
     assert check_all_state(res, res_box)
 
+test_amplitudes_local()
 
 def test_amplitudes_qusprout():
     """test set amp for qusprout"""
     # 使用qusprout量子计算模拟器
+    qubit_len = 3
     circuit = QCircuit(backend=BackendQuSprout())
-    qr = circuit.allocate(3)
+    qr = circuit.allocate(qubit_len)
     QSP("AMP", [1, 2, 3, 4, 5]) * qr
 
-    res = []
+    res = ['0, 0'] * (2**qubit_len)
     for i in range(len(circuit.init_amp_reals)): 
-        res.append(','.join([str(circuit.init_amp_reals[i]), str(circuit.init_amp_imags[i])]))
+        res[i] = ','.join([str(circuit.init_amp_reals[i]), str(circuit.init_amp_imags[i])])
 
     res_box = circuit.get_all_state()
 
