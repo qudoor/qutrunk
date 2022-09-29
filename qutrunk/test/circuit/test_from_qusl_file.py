4b3a1f8fb589ba9cbc8cb7672a7da58532926c09
wushuhua@qudoor.cn <wushuhua@qudoor.cn>
2022-09-29 16:03:03 +0800

    增加load,dump功能测试代码，完善qusl_parse的代码，完善meta.py中的反转电路代码

--- qutrunk/test/circuit/test_from_qusl_file.py
+++ qutrunk/test/circuit/test_from_qusl_file.py
@@ -1,23 +1,21 @@
 from qutrunk.circuit import QCircuit
 from qutrunk.circuit.gates import H, Measure, CNOT
 
+
 def test_qusl_file():
     qc = QCircuit()
     qreg = qc.allocate(2)
+
     H * qreg[0]
     CNOT * (qreg[0], qreg[1])
     Measure * qreg[0]
     Measure * qreg[1]
-    qc.print(file="a.qusl")
+
+    qc.print("a.qusl")
+
+    expect_in = qc.statements
+    circuit_in = qc.from_qusl_file(file="a.qusl")
+
+    assert expect_in == circuit_in.statements
 
 
-    # a.print(file="d.qusl")
-
-    # with open(r"D:\projects\qutrunk\qutrunk\test\circuit\d.qusl", "r") as stream:
-    #     container = stream.readline()
-    #
-    # circuit_in = container[container.rfind("code") - 1:-1]
-    # expect_in = '"code": ["H * q[0]\\n", "MCX * (q[0], q[1])\\n", "Measure * q[0]\\n", "Measure * q[1]\\n"]'
-
-if __name__ == "__main__":
-    test_qusl_file()
