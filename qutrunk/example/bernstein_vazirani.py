f047a73b1181ab8f0a49f509354bea6b72d5f42b
liuli <liuli@qudoor.com>
2022-10-24 11:05:51 +0800

    Implement bernstein vazirani algorithm

--- qutrunk/example/bernstein_vazirani.py
+++ qutrunk/example/bernstein_vazirani.py
@@ -1,43 +1,66 @@
 """Bernstein-Vazirani example."""
+import random
 
 from qutrunk.circuit import QCircuit
-from qutrunk.circuit.gates import CNOT, X
+from qutrunk.circuit.gates import CNOT, X, H
+from qutrunk.circuit.ops import Classical 
 
+PRECISION = 1e-10
 
-def run_bernstein_vazirani(backend=None):
-    num_qubits = 9
-    secret_num = 2**4 + 1
-    circuit = QCircuit(backend=backend, resource=True)
-
-    qureg = circuit.allocate(num_qubits)
-
-    X * qureg[0]
-
-    bits = secret_num
-    bit = 0
-    for qb in range(1, num_qubits):
+def apply_oracle(qr, num_qubits, secret):
+    bits = secret
+    for q in range(1, num_qubits):
+        # extract the (q-1)-th bit of secret
         bit = int(bits % 2)
         bits = int(bits / 2)
-        if bit:
-            CNOT * (qureg[0], qureg[qb])
+        
+        # NOT the ancilla, controlling on the q-th qubit
+        if (bit):
+            CNOT * (qr[q], qr[0])
 
-    success_prob = 1.0
-    bits = secret_num
-    for qb in range(1, num_qubits):
-        bit = int(bits % 2)
-        bits = int(bits / 2)
-        success_prob *= circuit.get_prob_outcome(qb, bit)
+def apply_bernstein_vazirani(num_qubits, secret):
+    # create circuit and qureg
+    circuit = QCircuit()
+    qr = circuit.allocate(num_qubits)
 
-    print(f"solution reached with probability {success_prob}")
-    circuit.print()
+    # start in |0>
+    Classical(0) * qr
 
-    circuit.run()
-    circuit.show_resource()
+    # NOT the ancilla
+    X * qr[0]
+
+    # H all qubits, including the ancilla
+    for q in range(num_qubits):
+        H * qr[q]
+
+    apply_oracle(qr, num_qubits, secret)
+
+    # H all qubits, including the ancilla
+    for q in range(num_qubits):
+        H * qr[q]
+
+    # infer the output basis state
+    ind = 2*secret + 1
+    prob = circuit.get_prob_amp(ind)
+    if 1 - prob < PRECISION:
+        prob = 1
+        
+    print(f"success probability:  {prob}")
 
     return circuit
 
-
 if __name__ == "__main__":
-    # local run
-    circuit = run_bernstein_vazirani()
+    # number of qubits
+    num_qubits = 15
+
+    # number of all states
+    num_elems = 2 ** (num_qubits - 1)
+
+    # randomly choose the secret parameter
+    secret = random.randint(0, num_elems - 1)
+
+    # search for s using BV's algorithm
+    circuit = apply_bernstein_vazirani(num_qubits, secret)
+
+    # draw the circuit
     circuit.draw()
