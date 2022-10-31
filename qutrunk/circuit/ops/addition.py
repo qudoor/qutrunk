186f4d4c5804db351bc5039b292373d7a5597862
wushuhua@qudoor.cn <wushuhua@qudoor.cn>
2022-10-31 09:43:59 +0800

    delete _add_statement() and OperatorContext.

--- qutrunk/circuit/ops/addition.py
+++ qutrunk/circuit/ops/addition.py
@@ -1,7 +1,7 @@
 """Addition operation."""
 
 from qutrunk.circuit import Qureg
-from qutrunk.circuit.ops.operator import Operator, OperatorContext
+from qutrunk.circuit.ops.operator import Operator
 from qutrunk.circuit.gates import X, MCX
 
 
@@ -34,24 +34,19 @@ class ADD(Operator):
             raise ValueError("number must be more than zero.")
         self.number = number
 
-    def _add_statement(self, qr):
-        qr[0].circuit.append_statement(f"ADD({self.number}) * q")
-
     def __mul__(self, qr: Qureg):
         if not isinstance(qr, Qureg):
             raise TypeError("The operand must be Qureg.")
 
         num_qubits = len(qr)
 
-        with OperatorContext(qr[0].circuit):
-            for _ in range(self.number):
+
+        for _ in range(self.number):
+            ctrl = []
+            for i in range(num_qubits, 1, -1):
+                for j in range(i - 1):
+                    ctrl.append(qr[j])
+                MCX(i - 1) * (*ctrl, qr[i - 1])
                 ctrl = []
-                for i in range(num_qubits, 1, -1):
-                    for j in range(i - 1):
-                        ctrl.append(qr[j])
-                    MCX(i - 1) * (*ctrl, qr[i - 1])
-                    ctrl = []
 
-                X * qr[0]
-
-        self._add_statement(qr)
+            X * qr[0]
