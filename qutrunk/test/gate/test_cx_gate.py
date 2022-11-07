c6c46f7034c590bec1d9b11d142c8adf2b2165d3
wushuhua@qudoor.cn <wushuhua@qudoor.cn>
2022-11-07 16:17:44 +0800

    add the testcase of CX gate.

--- qutrunk/test/gate/test_cx_gate.py
+++ qutrunk/test/gate/test_cx_gate.py
@@ -1,50 +1,19 @@
-import pytest
+import numpy as np
 
-from qutrunk.circuit import QCircuit
 from qutrunk.circuit.gates import CX
-from qutrunk.backends import BackendQuSprout
-from check_all_state import check_all_state
-from check_all_state_inverse import check_all_state_inverse
+from qutrunk.circuit import QCircuit
+from qutrunk.test.global_parameters import ZERO_STATE_2D
 
 
-def test_cx_gate():
-    """测试CX"""
-    # 使用本地量子计算模拟器
+def test_not_gate():
+    """Test CX gate."""
+    # local backend
     circuit = QCircuit()
     qr = circuit.allocate(2)
     CX * (qr[0], qr[1])
-    res = circuit.get_statevector()
-
-    # 使用BackendQuSprout量子计算模拟器
-    circuit_box = QCircuit(backend=BackendQuSprout())
-    qr_box = circuit_box.allocate(2)
-    CX * (qr_box[0], qr_box[1])
-    res_box = circuit_box.get_statevector()
-
-    # 检查数据是否一致
-    assert check_all_state(res, res_box)
-
-
-def test_cx_inverse_gate():
-    """测试反转电路"""
-    # 使用本地量子计算模拟器
-    circuit = QCircuit()
-    qr = circuit.allocate(2)
-    # 获取原始数据
-    org_res = circuit.get_statevector()
-
-    # 进行逆操作
     CX * (qr[0], qr[1])
-    CX * (qr[0], qr[1])
-    circuit.cmds[1].inverse = True
+    result = circuit.get_statevector()
+    result_backend = np.array(result)
 
-    # 获取逆操作后数据
-    final_res = circuit.get_statevector()
-
-    # 检查逆操作前后数据是否一致
-    assert check_all_state_inverse(org_res, final_res)
-
-
-if __name__ == "__main__":
-    """运行test文件"""
-    pytest.main(["-v", "-s", "./test_cx_gate.py"])
+    # initial state
+    assert np.allclose(result_backend, ZERO_STATE_2D)
--- qutrunk/test/global_parameters.py
+++ qutrunk/test/global_parameters.py
@@ -5,3 +5,4 @@ import numpy as np
 PRECISION = 1e-10
 # zero state
 ZERO_STATE = init_state = np.array([1, 0]).reshape(2, 1)
+ZERO_STATE_2D = init_state = np.array([[1, 0, 0, 0]])
