dc09c078d53e429e30e45f6e4c75f98dccd55936
liubo@qudoor.cn <liubo@qudoor.cn>
2022-11-21 17:51:11 +0800

    1.update qusaas backend of measure；

--- qutrunk/backends/qusaas/httpclient.py
+++ qutrunk/backends/qusaas/httpclient.py
@@ -117,7 +117,7 @@ class QuSaasApiServer:
         resp = resp.json()
 
         result = MeasureResult()
-        for meas in res["data"]["measures"]:
+        for meas in resp["data"]["measures"]:
             meas_temp = MeasureQubits()
             for mea in meas["measure"]:
                 mea_temp = MeasureQubit(mea["target"], mea["value"])
@@ -274,4 +274,4 @@ class QuSaasApiServer:
         saas_conf = configuration['QuSaas']
         url = f"{saas_conf['schema']}://{saas_conf['host']}:{saas_conf['port']}{saas_conf['api']['token']}"
         response = self._client.post(url, data=data).json()
-        return response['data']['access_token']
+        return response['access_token']
