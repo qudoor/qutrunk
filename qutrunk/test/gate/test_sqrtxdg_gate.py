e4a9ce5ffcfc1673242cc592184d9a9b267eae83
wushuhua@qudoor.cn <wushuhua@qudoor.cn>
2022-11-24 10:15:21 +0800

    add TestSqrtXdg class.

--- qutrunk/test/gate/test_sqrtxdg_gate.py
+++ qutrunk/test/gate/test_sqrtxdg_gate.py
@@ -1,39 +1,80 @@
 import numpy as np
+import pytest
+from qiskit import QuantumCircuit, assemble, Aer
 
-from qutrunk.circuit.gates import SqrtXdg
+from qutrunk.circuit.gates import H, All, SqrtXdg
 from qutrunk.circuit import QCircuit
-from qutrunk.test.global_parameters import ZERO_STATE
+from qutrunk.circuit.gates import Matrix
+from qutrunk.test.gate.backend_fixture import backend, backend_type
 
 
-def test_sqrtx_gate():
-    """Test SqrtXdg gate."""
-    # local backend
-    circuit = QCircuit()
-    qr = circuit.allocate(1)
-    SqrtXdg * qr[0]
-    result = circuit.get_statevector()
-    result_backend = np.array(result).reshape(-1, 1)
+class TestSqrtXdg:
+    @pytest.fixture
+    def result_qutrunk(self, backend):
+        # local backend
+        circuit = QCircuit(backend=backend)
+        qr = circuit.allocate(1)
+        All(H) * qr
+        SqrtXdg * qr[0]
+        result_q = np.array(circuit.get_statevector()).reshape(-1, 1)
+        return result_q
 
-    # math
-    result_math = np.dot(SqrtXdg.matrix, ZERO_STATE)
+    def test_matrix(self, result_qutrunk):
+        """Test SqrtXdg gate with Matrix."""
+        circuit = QCircuit()
+        qr = circuit.allocate(1)
+        Matrix(H.matrix.tolist()) * qr[0]
+        Matrix(SqrtXdg.matrix.tolist()) * qr[0]
+        result_m = circuit.get_statevector()
+        result_m = np.array(result_m).reshape(-1, 1)
 
-    assert np.allclose(result_backend, result_math)
+        assert np.allclose(result_qutrunk, result_m)
 
+    def test_qiskit(self, result_qutrunk):
+        """Test SqrtXdg gate with qiskit."""
+        qc = QuantumCircuit(1)
+        initial_state = [1, 0]
+        qc.initialize(initial_state, 0)
+        # apply gate
+        qc.h(0)
+        qc.sxdg(0)
+        # run
+        sim = Aer.get_backend("aer_simulator")
+        qc.save_statevector()
+        q_obj = assemble(qc)
+        result_qiskit = sim.run(q_obj).result()
+        result_qiskit = np.array(result_qiskit.get_statevector()).reshape(-1, 1)
 
-def test_sqrtx_inverse_gate():
-    """Test the inverse of SqrtXdg gate."""
-    # local backend
-    circuit = QCircuit()
-    qr = circuit.allocate(1)
-    SqrtXdg.inv() * qr[0]
-    result = circuit.get_statevector()
-    result_backend = np.array(result).reshape(-1, 1)
+        assert np.allclose(result_qutrunk, result_qiskit)
 
-    # math
-    m = 0.5 * np.array([
-        [[1 + 1j, 1 - 1j],
-         [1 - 1j, 1 + 1j]]
-    ])
-    result_math = np.dot(m, ZERO_STATE)
+    def test_gate_inverse(self):
+        """Test the inverse of SqrtXdg gate."""
+        # local backend
+        circuit = QCircuit()
+        qr = circuit.allocate(1)
+        All(H) * qr
+        # initial state
+        result_init = np.array(circuit.get_statevector()).reshape(-1, 1)
 
-    assert np.allclose(result_backend, result_math)
+        SqrtXdg * qr[0]
+        SqrtXdg.inv() * qr[0]
+        result_expect = circuit.get_statevector()
+        result_expect = np.array(result_expect).reshape(-1, 1)
+
+        assert np.allclose(result_init, result_expect)
+
+    def test_matrix_inverse(self):
+        """Test the inverse of SqrtXdg gate with Matrix."""
+        circuit = QCircuit()
+        qr = circuit.allocate(1)
+
+        Matrix(H.matrix.tolist()) * qr[0]
+        # initial state
+        result_init = np.array(circuit.get_statevector()).reshape(-1, 1)
+
+        Matrix(SqrtXdg.matrix.tolist()) * qr[0]
+        Matrix(SqrtXdg.matrix.tolist()).inv() * qr[0]
+        result_m = circuit.get_statevector()
+        result_m = np.array(result_m).reshape(-1, 1)
+
+        assert np.allclose(result_init, result_m)
