e89e75d7a6eefe2ac7151c3409cf8f454ec2d229
wushuhua@qudoor.cn <wushuhua@qudoor.cn>
2022-12-02 09:57:04 +0800

    add HybridFunction class.

--- qutrunk/example/qutrunk_pytorch.py
+++ qutrunk/example/qutrunk_pytorch.py
@@ -15,10 +15,6 @@ from qutrunk.circuit.gates import H, Ry, Measure, Barrier
 
 
 class QuantumCircuit:
-    """
-    This class provides a simple interface for interaction
-    with the quantum circuit
-    """
 
     def __init__(self, n_qubits, backend=None, shots=100):
         # 定义量子电路
@@ -68,7 +64,43 @@ class QuantumCircuit:
         return np.array([expectation])
 
 
+class HybridFunction(Function):
+    """ Hybrid quantum - classical function definition """
+
+    @staticmethod
+    def forward(ctx, input, quantum_circuit, shift):
+        """ Forward pass computation """
+        ctx.shift = shift
+        ctx.quantum_circuit = quantum_circuit
+
+        expectation_z = ctx.quantum_circuit.run(input[0].tolist())
+        result = torch.tensor([expectation_z])
+        ctx.save_for_backward(input, result)
+
+        return result
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        """ Backward pass computation """
+        input, expectation_z = ctx.saved_tensors
+        input_list = np.array(input.tolist())
+
+        shift_right = input_list + np.ones(input_list.shape) * ctx.shift
+        shift_left = input_list - np.ones(input_list.shape) * ctx.shift
+
+        gradients = []
+        for i in range(len(input_list)):
+            expectation_right = ctx.quantum_circuit.run(shift_right[i])
+            expectation_left = ctx.quantum_circuit.run(shift_left[i])
+
+            gradient = torch.tensor([expectation_right]) - torch.tensor([expectation_left])
+            gradients.append(gradient)
+        gradients = np.array([gradients]).T
+        return torch.tensor([gradients]).float() * grad_output.float(), None, None
+
+
 if __name__ == '__main__':
     circuit = QuantumCircuit(1, None, 100)
     # circuit.run({"theta": np.pi})
     print(f'Expected value for rotation pi= {circuit.run({"theta": np.pi})[0]}')
+    circuit._circuit.draw()
