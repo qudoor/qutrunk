be256dfd5c35c4a07e980934dedd4b81f5189653
wushuhua@qudoor.cn <wushuhua@qudoor.cn>
2022-12-05 10:42:34 +0800

    add neural network class.

--- qutrunk/example/qutrunk_pytorch/hybrid_nn.py
+++ qutrunk/example/qutrunk_pytorch/hybrid_nn.py
@@ -57,3 +57,26 @@ class Hybrid(nn.Module):
 
     def forward(self, input):
         return HybridFunction.apply(input, self.quantum_circuit, self.shift)
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 6, kernel_size=5)
+        self.conv2 = nn.Conv2d(6, 16, kernel_size=5)
+        self.dropout = nn.Dropout2d()
+        self.fc1 = nn.Linear(256, 64)
+        self.fc2 = nn.Linear(64, 1)
+        self.hybrid = Hybrid(None, 100, np.pi / 2)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.max_pool2d(x, 2)
+        x = F.relu(self.conv2(x))
+        x = F.max_pool2d(x, 2)
+        x = self.dropout(x)
+        x = x.view(1, -1)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        x = self.hybrid(x)
+        return torch.cat((x, 1 - x), -1)
