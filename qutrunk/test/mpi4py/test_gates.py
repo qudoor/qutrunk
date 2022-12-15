e1e6756ceb81e6e352e790499744c4dff0ee327d
guomengjie <guomengjie@qudoor.cn>
2022-12-15 15:10:19 +0800

    bugfix: verify u3, u2, u1, crx, cry, crz distribute sim by mpi4py

--- qutrunk/test/mpi4py/test_gates.py
+++ qutrunk/test/mpi4py/test_gates.py
@@ -1,8 +1,9 @@
+import math
 import sys
 import trace
 
 from qutrunk.circuit import QCircuit
-from qutrunk.circuit.gates import H, Swap, Sdg, Tdg, CNOT, MCX, All, CY, MCZ
+from qutrunk.circuit.gates import H, Swap, Sdg, Tdg, CNOT, MCX, All, CY, MCZ, U3, U2, U1, CRx, CRy, CRz
 
 
 def test_swap():
@@ -62,6 +63,52 @@ def test_mcz():
     print(cir.get_statevector())
 
 
+def test_u3():
+    cir = QCircuit()
+    qr = cir.allocate(1)
+    U3(math.pi / 2, 0, math.pi) * qr[0]
+    print(cir.get_statevector())
+
+
+def test_u2():
+    cir = QCircuit()
+    qr = cir.allocate(1)
+    U2(0, math.pi) * qr[0]
+    print(cir.get_statevector())
+
+
+def test_u1():
+    cir = QCircuit()
+    qr = cir.allocate(1)
+    H * qr[0]
+    U1(math.pi / 4) * qr[0]
+    print(cir.get_statevector())
+
+
+def test_crx():
+    cir = QCircuit()
+    qr = cir.allocate(2)
+    All(H) * qr
+    CRx(math.pi / 2) * (qr[0], qr[1])
+    print(cir.get_statevector())
+
+
+def test_cry():
+    cir = QCircuit()
+    qr = cir.allocate(2)
+    All(H) * qr
+    CRy(math.pi / 2) * (qr[0], qr[1])
+    print(cir.get_statevector())
+
+
+def test_crz():
+    cir = QCircuit()
+    qr = cir.allocate(2)
+    All(H) * qr
+    CRz(math.pi / 2) * (qr[0], qr[1])
+    print(cir.get_statevector())
+
+
 # define Trace object: trace line numbers at runtime, exclude some modules
 tracer = trace.Trace(
     ignoredirs=[sys.prefix, sys.exec_prefix],
@@ -76,4 +123,4 @@ tracer = trace.Trace(
 # redirect to a different file for each processes
 # sys.stdout = open('trace_{:04d}.txt'.format(COMM_WORLD.rank), 'w')
 
-tracer.runfunc(test_mcz)
+tracer.runfunc(test_crz)
