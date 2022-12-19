8f7f0375605f1d71553a48a920f12afeccc637a2
liuli <liuli@qudoor.com>
2022-12-19 14:25:44 +0800

    auto check and add some python mpi test cases

--- qutrunk/test/gate/mpi/main.py
+++ qutrunk/test/gate/mpi/main.py
@@ -1,6 +1,18 @@
 import os  
-main = "mpiexec -n 2 python qutrunk/test/gate/mpi/test_ch_gate.py"
-os.system(main) 
 
-main = "mpiexec -n 2 python qutrunk/test/gate/mpi/test_cp_gate.py"
-os.system(main)
+def test_mpi(num_rank):
+    file = 'qutrunk/test/gate/mpi/'
+
+    for root, dirs, files in os.walk(file):
+        if root != file:
+            break
+
+        for file in files:
+            if file == 'main.py':
+                continue
+            path = os.path.join(root, file)
+            cmd = "mpiexec -n {} python {}".format(num_rank, path)
+            os.system(cmd)
+
+if __name__ == "__main__":
+    test_mpi(2)
--- qutrunk/test/gate/mpi/test_cu1_gate.py
+++ qutrunk/test/gate/mpi/test_cu1_gate.py
@@ -3,28 +3,28 @@ from numpy import pi
 import trace
 import sys
 
-from qutrunk.circuit.gates import CP, All, H
+from qutrunk.circuit.gates import CU1, All, H
 from qutrunk.circuit import QCircuit
 from qutrunk.backends import BackendLocal
 
-def test_cp_gate():
+def test_cu1_gate():
     circuit = QCircuit()
     qr = circuit.allocate(2)
     All(H) * qr
-    CP(pi / 2) * (qr[0], qr[1])
+    CU1(pi / 2) * (qr[0], qr[1])
     result = np.array(circuit.get_statevector()).reshape(-1, 1)
-
+    
     circuit_mpi = QCircuit(backend=BackendLocal('mpi'))
     qr_mpi = circuit_mpi.allocate(2)
     All(H) * qr_mpi
-    CP(pi / 2) * (qr_mpi[0], qr_mpi[1])
+    CU1(pi / 2) * (qr_mpi[0], qr_mpi[1])
     result_mpi = np.array(circuit_mpi.get_statevector()).reshape(-1, 1)
-    
+
     if circuit_mpi.backend._local_impl.sim.reg.chunk_id == 0:
         if np.allclose(result, result_mpi):
-            print('test_cp_gate: T')
+            print('test_cu1_gate: T')
         else:
-            print('test_cp_gate: F')
+            print('test_cu1_gate: F')
 
 
 # define Trace object: trace line numbers at runtime, exclude some modules
@@ -41,4 +41,4 @@ tracer = trace.Trace(
 # redirect to a different file for each processes
 # sys.stdout = open('trace_{:04d}.txt'.format(COMM_WORLD.rank), 'w')
 
-tracer.runfunc(test_cp_gate)
+tracer.runfunc(test_cu1_gate)
