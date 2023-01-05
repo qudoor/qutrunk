327ba5571025e0f2b160e6d72b3b828f46de81cb
guomengjie <guomengjie@qudoor.cn>
2023-01-05 11:05:35 +0800

    feat: BACKEND_TYPE from sys env, key shall be upper case

--- qutrunk/tools/env_reader.py
+++ qutrunk/tools/env_reader.py
@@ -5,18 +5,18 @@ from qutrunk.backends import BackendLocal, BackendQuSprout
 
 
 def backend_from_env():
-    be_type = os.getenv("backend_type", "local")
+    be_type = os.getenv("BACKEND_TYPE", "local")
     be_type = be_type.lower()
     if be_type == "local":
         return BackendLocal()
     elif be_type == "qusprout":
-        run_mode = os.getenv("run_mode", "cpu")
-        backend_ip = os.getenv("backend_ip")
-        backend_port = os.getenv("backend_port")
+        run_mode = os.getenv("RUN_MODE", "cpu")
+        backend_ip = os.getenv("BACKEND_IP")
+        backend_port = os.getenv("BACKEND_PORT")
         return BackendQuSprout(run_mode, backend_ip, backend_port)
     elif be_type == "aws_braket":
         from qutrunk.backends.braket import BackendAWSDevice
-        device_name = os.getenv("device_name", "SV1")
+        device_name = os.getenv("DEVICE_NAME", "SV1")
         return BackendAWSDevice(device_name)
         # return BackendAWSLocal()
     else:
