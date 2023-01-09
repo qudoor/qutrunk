6c30cb49eb3482c85ba1eb9b6f9249278e867fa4
liubo@qudoor.cn <liubo@qudoor.cn>
2023-01-09 14:48:47 +0800

    1.add init state func of gpu backend.

--- qutrunk/backends/local/sim_gpu.py
+++ qutrunk/backends/local/sim_gpu.py
@@ -6,6 +6,49 @@ import numpy
 from numba import cuda, float32
 from .sim_local import SimLocal, PauliOpType
 
+@cuda.jit
+def init_classical_state_kernel(num_amps_per_rank, real, imag, state_ind):
+    """Init classical state kernel"""
+    index = cuda.threadIdx.x + cuda.blockDim.x * cuda.blockIdx.x
+    if index < num_amps_per_rank:
+        real[index] = 0.0
+        imag[index] = 0.0
+    if index == state_ind:
+        real[state_ind] = 1.0
+        imag[state_ind] = 0.0
+    cuda.syncthreads()
+    
+@cuda.jit
+def init_plus_state_kernel(num_amps_per_rank, real, imag):
+    """Init plus state kernel"""
+    index = cuda.threadIdx.x + cuda.blockDim.x * cuda.blockIdx.x
+    norm_factor = 1.0/math.sqrt(num_amps_per_rank)
+    if index < num_amps_per_rank:
+        real[index] = norm_factor
+        imag[index] = 0.0
+    cuda.syncthreads()
+    
+@cuda.jit  
+def init_zero_state_kernel(num_amps_per_rank, real, imag):
+    """Init zero state kernel"""
+    index = cuda.threadIdx.x + cuda.blockDim.x * cuda.blockIdx.x
+    if index < num_amps_per_rank:
+        real[index] = 0.0
+        imag[index] = 0.0
+    if index == 0:
+        real[0] = 1.0
+        imag[0] = 0.0
+    cuda.syncthreads()
+
+@cuda.jit  
+def amp_kernel(num_amps_per_rank, real, imag, orgreal, orgimag, startindex):
+    """Init zero state kernel"""
+    index = cuda.threadIdx.x + cuda.blockDim.x * cuda.blockIdx.x
+    if index < num_amps_per_rank:
+        endindex = startindex+index
+        real[endindex] = orgreal[index]
+        imag[endindex] = orgimag[index]
+    cuda.syncthreads()
 
 class GpuLocal:
     """Simulator-gpu implement."""
@@ -14,39 +57,34 @@ class GpuLocal:
         self.sim_cpu = None
     
     def create_qureg(self, num_qubits):  
+        self.sim_cpu = SimLocal()
         self.sim_cpu.create_qureg(num_qubits)
         num_amps = 2**num_qubits
-        self.real = cuda.shared.array(num_amps, dtype=float32)
-        self.imag = cuda.shared.array(num_amps, dtype=float32)
-        
-    @cuda.jit
-    def init_classical_state_kernel(self, state_ind):
-        """Init classical state kernel"""
-        index = cuda.threadIdx.x + cuda.blockDim.x * cuda.blockIdx.x
-        if index < self.sim_cpu.num_amps_per_rank:
-            self.real[index] = 0.0
-            self.imag[index] = 0.0
-        if index == state_ind:
-            self.real[state_ind] = 1.0
-            self.imag[state_ind] = 0.0
+        self.real = cuda.device_array(num_amps, numpy.float_)
+        self.imag = cuda.device_array(num_amps, numpy.float_)
 
     def init_classical_state(self):
         """Init classical state"""
         threads_per_block = 128
         blocks_per_grid = math.ceil(self.sim_cpu.num_amps_per_rank / threads_per_block)
-        self.init_classical_state_kernel[blocks_per_grid, threads_per_block]()
-   
-    @cuda.jit
-    def init_plus_state_kernel(self):
-        """Init plus state kernel"""
-        index = cuda.threadIdx.x + cuda.blockDim.x * cuda.blockIdx.x
-        norm_factor = 1.0/math.sqrt(self.sim_cpu.num_amps_per_rank)
-        if index < self.sim_cpu.num_amps_per_rank:
-            self.real[index] = norm_factor
-            self.imag[index] = 0.0
+        init_classical_state_kernel[blocks_per_grid, threads_per_block](self.sim_cpu.num_amps_per_rank, self.real, self.imag, 0)
             
     def init_plus_state(self):
         """Init plus state"""
         threads_per_block = 128
         blocks_per_grid = math.ceil(self.sim_cpu.num_amps_per_rank / threads_per_block)
-        self.init_plus_state_kernel[blocks_per_grid, threads_per_block]()
+        init_plus_state_kernel[blocks_per_grid, threads_per_block](self.sim_cpu.num_amps_per_rank, self.real, self.imag)
+    
+    def init_zero_state(self):
+        """Init zero state"""
+        threads_per_block = 128
+        blocks_per_grid = math.ceil(self.sim_cpu.num_amps_per_rank / threads_per_block)
+        init_zero_state_kernel[blocks_per_grid, threads_per_block](self.sim_cpu.num_amps_per_rank, self.real, self.imag)
+
+    def amp(self, reals, imags, startindex):
+        """Init amplitudes state"""
+        orgreal = cuda.to_device(reals, stream=0)
+        orgimag = cuda.to_device(imags, stream=0)
+        threads_per_block = 128
+        blocks_per_grid = math.ceil(self.sim_cpu.num_amps_per_rank / threads_per_block)
+        amp_kernel[blocks_per_grid, threads_per_block](self.sim_cpu.num_amps_per_rank, self.real, self.imag, orgreal, orgimag, startindex)
