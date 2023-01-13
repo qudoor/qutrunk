ab7654451f2f903b15abfea3fdde84fbc1eef717
liubo@qudoor.cn <liubo@qudoor.cn>
2023-01-13 09:22:36 +0800

    1.add pytest in gpu backend.

---
+++ qutrunk/test/gate/gpu/test_SqrtSwap_gate.py
@@ -0,0 +1,21 @@
+import pytest
+import numpy as np
+
+from qutrunk.circuit.gates import SqrtSwap, All, H
+from qutrunk.circuit import QCircuit
+from qutrunk.backends import BackendLocal
+
+def test_SqrtSwap_gate():
+    circuit = QCircuit()
+    qr = circuit.allocate(3)
+    All(H) * qr
+    SqrtSwap * (qr[0], qr[1])
+    result = np.array(circuit.get_statevector()).reshape(-1, 1)
+
+    circuit_gpu = QCircuit(backend=BackendLocal("gpu"))
+    qr_gpu = circuit_gpu.allocate(3)
+    All(H) * qr_gpu
+    SqrtSwap * (qr_gpu[0], qr_gpu[1])
+    result_gpu = np.array(circuit_gpu.get_statevector()).reshape(-1, 1)
+
+    assert np.allclose(result, result_gpu)
